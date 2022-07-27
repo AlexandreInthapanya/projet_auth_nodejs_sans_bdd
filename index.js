8cdff0618809f0ae074b3318a9d008c5251f900c
Alex <anou_web@hotmail.com>
2022-07-27 12:32:40 +0200

    creation template et route en cours

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require("express")
+const app = express();
+
+
+
+app.get('/login', (req, res) => {
+    res.render('login', {title : 'page de Connexion'}) //render pour utilisation template login.ejs
+})
+
+app.post('/login', (req, res) => { //resultat quand utilisateur click sur le boutton submit
+    
+})
